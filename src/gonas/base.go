cc9e1f21e2411520d68855f1ce6f42afd27a14d1
joamag <joamag@gmail.com>
2014-07-09 09:57:26 +0100

    new comments

--- src/gonas/base.go
+++ src/gonas/base.go
@@ -57,17 +57,28 @@ func (srv *AbstractServer) Init() error {
 
 func Serve(srv Server) error {
     srv.Init()
+
+    // prints an information message about the starting
+    // of gonas and the name of the server to be used
     srv.Infof("Starting gonas %s main loop ...\n", srv.Name())
 
+    // creates the listener on the usual host and port
+    // and using a tcp listening on all protocols
     ln, err := net.Listen("tcp", "0.0.0.0:8080")
     if err != nil {
         return err
     }
 
+    // defers the closing of the listener to the end of the
+    // function so that it does not remaing open (garbage)
     defer ln.Close()
 
+    // prints a message about the starting of the connections
+    // acceptance for the current server
     srv.Tracef("Accepting %s connections ...\n", srv.Name())
 
+    // runs the service main loop accepting new connections and
+    // redirecting its handling to the proper server
     for {
         conn, err := ln.Accept()
         if err != nil {
@@ -77,5 +88,7 @@ func Serve(srv Server) error {
         srv.Handle(conn)
     }
 
+    // returns an invalid value as no error has occurred for the
+    // current service handling infra-structure (everything ok)
     return nil
 }
