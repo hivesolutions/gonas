852b1356fbfe127bdb1f6a7f635fd9a3bbf22552
joamag <joamag@gmail.com>
2015-12-28 09:36:32 +0000

    small comment change

--- src/gonas/base.go
+++ src/gonas/base.go
@@ -6,7 +6,7 @@ import "net"
 import "io/ioutil"
 
 // Server Interface that defines a proper server implementation
-// note that simplicity is one of the major concerns
+// note that simplicity is one of the major concerns.
 type Server interface {
     Logger
 
